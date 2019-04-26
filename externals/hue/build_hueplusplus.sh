feb6de27a4da9d83832147d67a00e59f0b487272
Leo An <leo@innoaus.com>
2019-04-26 10:16:33 +0900

    .

--- externals/hue/build_hueplusplus.sh
+++ externals/hue/build_hueplusplus.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/enwi/hueplusplus.git
 cd hueplusplus
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/git-innoaus/prebuilt_externals/$1/devtools
+cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../../../../$1/devtools
 make -j 8
 make install
