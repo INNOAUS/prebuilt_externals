9c2d18b7c48d4c6a307372905d2577b6c5fe4fa0
Leo An <leo@innoaus.com>
2019-05-03 06:59:59 +0900

    .

--- externals/cryptopp/build_raspbian.sh
+++ externals/cryptopp/build_raspbian.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#git clone https://github.com/weidai11/cryptopp.git
+git clone https://github.com/weidai11/cryptopp.git
 cd cryptopp
 #export CXXFLAGS="-fPIC -DNDEBUG -DCRYPTOPP_NO_UNALIGNED_DATA_ACCESS -g2 -O3 -std=c++11"
 export CXXFLAGS="-fPIC -DNDEBUG -DCRYPTOPP_NO_UNALIGNED_DATA_ACCESS -g2 -O3 -std=c++11"
 make -j8 static cryptest.exe
-make -j8 static cryptest.exe install PREFIX=$HOME/git-innoaus/prebuilt_externals/raspbian/devtools
+make -j8 static cryptest.exe install PREFIX=/git-innoaus/prebuilt_externals/raspbian/devtools
 
