03d85389503a4f47ff9358d7ddaabdc2ead726cc
Leo An <leo@innoaus.com>
2019-05-02 11:59:38 +0900

    .

--- externals/cryptopp/build.sh
+++ externals/cryptopp/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CXXFLAGS="-fPIC -DNDEBUG -g2 -O3 -std=c++11"
-make -j8 static cryptest.exe
-make -j8 static cryptest.exe install PREFIX=$HOME/git-innoaus/prebuilt_externals/$1/devtools
+make -j8 static
+make -j8 static install PREFIX=$HOME/git-innoaus/prebuilt_externals/$1/devtools
 
