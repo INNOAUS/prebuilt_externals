9be8b2caa5d8e658574ddd60f14862bf13a0baa2
Leo An <leo@innoaus.com>
2019-05-03 07:26:18 +0900

    .

--- externals/hue/build_hueplusplus_raspbian.sh
+++ externals/hue/build_hueplusplus_raspbian.sh
@@ -2,6 +2,6 @@ git clone --recursive https://github.com/enwi/hueplusplus.git
 cd hueplusplus
 rm -rf build
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/git-innoaus/prebuilt_externals/$1/devtools
+cmake .. -DCMAKE_INSTALL_PREFIX=/git-innoaus/prebuilt_externals/raspbian/devtools
 make -j 8
 make install
