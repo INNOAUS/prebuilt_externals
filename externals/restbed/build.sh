63bd317935a9dfc12ee73748ff24e8dce9327194
Leo An <leo@innoaus.com>
2019-05-07 09:58:37 +0900

    .

--- externals/restbed/build.sh
+++ externals/restbed/build.sh
@@ -1,4 +1,4 @@
-#git clone --recursive https://github.com/Corvusoft/restbed.git
+git clone --recursive https://github.com/Corvusoft/restbed.git
 cd restbed
 mkdir build && cd build
 cmake .. -DBUILD_SSL=NO -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_INSTALL_PREFIX=$PWD/../../../../$1/devtools
