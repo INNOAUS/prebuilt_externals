9e796f7d6723d641534a5f65abaed53298c45620
Leo An <leo@innoaus.com>
2019-05-08 14:59:04 +0900

    .

--- centos/devtools/lib/prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-tar xvfj libACE.a.tar.bz2
-tar xvfj libcryptopp.a.tar.bz2
-tar xvfj librestbed.a.tar.bz2
-
--- centos/devtools/lib64/prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-tar xvfj libIce++11.a.tar.bz2
-tar xvfj libIceGrid++11.a.tar.bz2
-tar xvfj libIce.a.tar.bz2
-
