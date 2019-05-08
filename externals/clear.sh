59cf1f45df981c6663c7ca0bd02894b25f6bd879
Leo An <leo@innoaus.com>
2019-05-08 14:19:11 +0900

    .

---
+++ externals/clear.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf CLI11/CLI11
+rm -rf ace/ACE_wrappers
+rm -rf bzip2/bzip2
+rm -rf cctz/cctz
+rm -rf cpp_redis/cpp_redis
+rm -rf cryptopp/cryptopp
+rm -rf docopt/docopt.cpp
+rm -rf expat/libexpat
+rm -rf fmt/fmt
+rm -rf hue/hueplusplus
+rm -rf json/json
+rm -rf libqrencode/libqrencode
+rm -rf mcpp/mcpp
+rm -rf restbed/restbed
+rm -rf zeroc-ice/ice
---
+++ raspbian/devtools/lib/clear.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm libACE.a
+rm libcryptopp.a
+rm libIce++11.a
+rm librestbed.a
