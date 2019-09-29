3a42ce38596b4ff5524031778c72c9e0cde1c528
Leo An <leo@innoaus.com>
2019-09-29 13:18:39 +0900

    add redis builds

---
+++ redis-build/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export INSTALLDIR=$PWD
+tar xvf redis-5.0.5.tar.gz
+pushd redis-5.0.5
+make -j8
+make test
+make install PREFIX=$INSTALLDIR/redis-server
+cp redis.conf $INSTALLDIR/redis-server/bin
+popd
