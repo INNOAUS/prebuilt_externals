8be90c6304542b72b147701f427651fc3a273f51
Leo An <leo@innoaus.com>
2019-09-29 10:41:23 +0900

    add bdr build from source

---
+++ bdr-build/build.centos.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# This script is part of the BDR quick-start guide.
+#
+
+tar xvfj bdr-pg-src.tar.bz2
+tar xvfj bdr-plugin-src.tar.bz2
+
+DESTDIR=$PWD/bdr
+pushd bdr-pg-src
+./configure --prefix=$DESTDIR 2>&1 | tee -a "$LOGFILE"
+make -s | tee -a "$LOGFILE"
+make -s install 2>&1 | tee -a "$LOGFILE"
+make -s -C contrib 2>&1 | tee -a "$LOGFILE"
+make -s -C contrib install 2>&1 | tee -a "$LOGFILE"
+popd
+
+echo "PostgreSQL for BDR compiled and installed."
+echo "Preparing BDR extension..."
+
+pushd bdr-plugin-src
+PATH=$DESTDIR/bin:$PATH ./configure 2>&1 | tee -a "$LOGFILE"
+make -s 2>&1 | tee -a "$LOGFILE"
+make -s install 2>&1 | tee -a "$LOGFILE"
+popd
+
+echo
+echo "---------------------------"
+echo "BDR compiled and installed."
+echo
+echo "Sources at $BASEDIR/bdr-src"
+echo "Installed to $BASEDIR/bdr"
+echo
+echo "Now add it to your PATH:"
+echo "    export PATH=$BASEDIR/bdr/bin:\$PATH"
+echo "and carry on with the quickstart in the documentation."
+echo
+echo "WARNING: this is just a toy BDR install for testing and experimentation!"
+echo
+echo "---------------------------"
+
+trap "" EXIT
---
+++ bdr-build/clear.sh
@@ -0,0 +1,3 @@
+rm -rf bdr
+rm -rf bdr-pg-src
+rm -rf bdr-plugin-src
