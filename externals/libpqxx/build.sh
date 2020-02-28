871947adaebdc41185bb04ba36f66c5b4505a4df
Leo An <leo@innoaus.com>
2020-02-28 13:32:31 +0900

    .

--- externals/libpqxx/build.sh
+++ externals/libpqxx/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+export PATH=$HOME/git-innoaus/prebuilt_libs_$1/devtools/bdr/bin:$PATH
+export PostgresSQL_ROOT=$HOME/git-innoaus/prebuilt_libs_$1/devtools/bdr
+
 git clone -b 6.3 --recursive https://github.com/jtv/libpqxx.git
 mkdir -p libpqxx/build
 pushd libpqxx/build
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/git-innoaus/prebuilt_externals/$1/devtools
+cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/git-innoaus/prebuilt_libs_$1/devtools
 #./configure CXXFLAGS=-fPIC --prefix=$HOME/git-innoaus/prebuilt_externals/$1/devtools
 make install
 popd
