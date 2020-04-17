c37218306a5e38ef8554c179ebea5d4baa984771
Leo An <leo@innoaus.com>
2020-04-17 22:23:34 +0900

    fix build script

--- externals/machineid/build.sh
+++ externals/machineid/build.sh
@@ -6,8 +6,9 @@ mkdir -p machineid/build
 cd machineid/build
 
 if [ $1 == 'macosx' ]
-    cmake .. -DCMAKE_CXX_FLAGS=-DDARWIN -DCMAKE_INSTALL_PREFIX=$HOME/git-innoaus/prebuilt_libs_$1/devtools
 then
+    cmake .. -DCMAKE_CXX_FLAGS=-DDARWIN -DCMAKE_INSTALL_PREFIX=$HOME/git-innoaus/prebuilt_libs_$1/devtools
+else
     cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/git-innoaus/prebuilt_libs_$1/devtools
 fi
 
