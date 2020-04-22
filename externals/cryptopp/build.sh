6201b377250962d2413436c6a9b640bfd2f4326e
Leo An <leo@innoaus.com>
2020-04-22 21:02:54 +0900

    .

--- externals/cryptopp/build.sh
+++ externals/cryptopp/build.sh
@@ -3,5 +3,5 @@ git clone https://github.com/weidai11/cryptopp.git
 cd cryptopp
 export CXXFLAGS="-fPIC -DNDEBUG -g2 -O3 -std=c++11"
 make -j3 static
-make -j3 static install PREFIX=$HOME/git-innoaus/prebuilt_externals/$1/devtools
+make -j3 static install PREFIX=$HOME/git-innoaus/prebuilt_libs_$1/devtools
 
--- externals/restbed/build.sh
+++ externals/restbed/build.sh
@@ -1,6 +1,6 @@
 git clone --recursive https://github.com/Corvusoft/restbed.git
 cd restbed
 mkdir build && cd build
-cmake .. -DBUILD_SSL=YES -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_INSTALL_PREFIX=$PWD/../../../../$1/devtools
+cmake .. -DBUILD_SSL=YES -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_INSTALL_PREFIX=$HOEM/git-innoaus/prebuilt_libs_$1/devtools
 make -j2
 make install
