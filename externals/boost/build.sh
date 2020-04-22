821cbac8401dd32b1c5504696f61db52d4c1924c
Leo An <leo@innoaus.com>
2020-04-22 21:35:55 +0900

    .

--- externals/boost/build.sh
+++ externals/boost/build.sh
@@ -1,2 +1,3 @@
+cd boost
 ./bootstrap.sh --prefix=$HOME/git-innoaus/prebuilt_libs_$1/devtools
 ./b2 --prefix=$HOME/git-innoaus/prebuilt_libs_$1/devtools link=static threading=multi variant=release runtime-link=static install
